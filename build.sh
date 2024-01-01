42d2a43cbce55251b4a80a4713762f9a967751cd
Tim Hopper <tim.hopper@dtn.com>
2024-01-01 17:10:33 -0500

    make basic search work

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pip install -r requirements.txt
+mkdir -p site/content/churches
+python extract2.py
+cd site
+npm install
+node build-index.js > static/search_index.json
+hugo --gc --minify -b ${DEPLOY_PRIME_URL}
---
+++ site/build-index.js
@@ -0,0 +1,58 @@
+const fs = require('fs').promises;
+var read = require('fs-readdir-recursive')
+const { promisify } = require('util');
+const frontMatterParser = require('parser-front-matter');
+
+const parse = promisify(frontMatterParser.parse.bind(frontMatterParser));
+
+async function loadPostsWithFrontMatter(postsDirectoryPath) {
+  const postNames = read(postsDirectoryPath);
+  const posts = await Promise.all(
+    postNames.map(async fileName => {
+      const fileContent = await fs.readFile(
+        `${postsDirectoryPath}/${fileName}`,
+        'utf8'
+      );
+      const { content, data } = await parse(fileContent);
+      return {
+        content: content.slice(0, 3000),
+        ...data,
+        ministers: data.ministers ? data.ministers.join(', ') : undefined,
+        location: data.location ? `${data.location.city} ${data.location.state} ${data.location.address ? ', ' + data.location.address : ''}` : undefined,
+        names: data.names ? data.names.map(item => item.name).join(', ') : undefined,
+      };
+    })
+  );
+  return posts;
+}
+
+const lunrjs = require('lunr');
+
+function makeIndex(posts) {
+  return lunrjs(function () {
+    this.ref('title');
+    this.field('title');
+    this.field('ministers');
+    this.field('content');
+    this.field('location');
+    this.field('names');
+    this.field('raw_data');
+    posts.forEach(p => {
+      this.add(p);
+    });
+  });
+}
+
+async function run() {
+  const posts = await (await loadPostsWithFrontMatter(`${__dirname}/content`));
+  //console.log(JSON.stringify(posts));
+  const index = makeIndex(posts);
+  console.log(JSON.stringify(index));
+}
+
+run()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error.stack);
+    process.exit(1);
+  });
